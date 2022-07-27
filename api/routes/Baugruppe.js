062d7d7123d9eca0694a6f615c7f386fd24e06f6
graityr <89629439+graityr@users.noreply.github.com>
2022-07-27 13:40:59 +0200

    added Baugruppe.js code (copied from Bauelement.js)

--- api/routes/Baugruppe.js
+++ api/routes/Baugruppe.js
@@ -1,14 +1,42 @@
 const express = require('express');
-
 const router = express.Router();
+const Baugruppe = require('../models/Baugruppe');
 
 //Routes
-router.get('/', (req, res) =>
-    res.send(`Baugruppe`)
-)
+router.get('/', async (req, res) => {
+    try {
+        const getBaulement = await Baugruppe.find();
+        res.json(getBaulement);
+    } catch (err) {
+        res.json({message: err});
+    }
+});
 
-router.get('/test', (req, res) =>
-    res.send(`test Baugruppe`)
-)
+router.post('/', async (req, res) => {
+    const newBaugruppe = new Baugruppe({
+        name: req.body.name,
+        gewicht: req.body.gewicht,
+        laenge: req.body.laenge,
+        breite: req.body.breite,
+        hoehe: req.body.hoehe,
+    });
+
+    try {
+    const savedBaugruppe = await newBaugruppe.save();
+        res.json(savedBaugruppe);
+        } catch(err) {
+            res.json({message: err});
+        }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const delBaulement = await Baugruppe.deleteOne({_id: req.params.id});
+        res.json(delBaulement);
+    } catch (err) {
+        res.json({message: err});
+    }
+});
+
 
 module.exports = router;
